5064249640a5fe4ee521ecc4a8bf6c83f1bd8c61
Marcia Belen <alvarezmarciabelen@gmail.com>
2022-12-06 19:37:59 -0300

    Arreglos visuales en adivinador. Grabaciones de scripts funcionando. loop en el bot

--- adivinador/adivinador.sh
+++ adivinador/adivinador.sh
@@ -1,4 +1,5 @@
 #Comienzo del script
+clear
 echo "Bienvenidx al script adivinidador, nos vamos a divertir..."
 num_aleatorio=$(( $RANDOM % 50 + 1 ))
 
@@ -12,15 +13,16 @@ while [ "$numero" -ne "$num_aleatorio" ];
 do
 	echo "Ingrese un numero para intentar! Luego presione enter..."
 	read numero
+	clear
 	if [ "$numero" -gt "0" ] && [ "$numero" -le "50" ];
-	then
+		then
 		cant_intentos=$((cant_intentos+1))
 		if [ "$numero" -eq "$num_aleatorio" ];
-			then if [ "$cant_intentos" -le "5" ];
-				then
-					 echo "Adivinaste, felicidades. Estas dentro del 0,001 más inteligente de la poblacion...Si, esta chequeadisimo"
+				then if [ "$cant_intentos" -le "5" ];
+					then
+					echo "Adivinaste, felicidades. Estas dentro del 0,001 más inteligente de la poblacion...Si, esta chequeadisimo"
 			     	elif [ "$cant_intentos" -gt "5" ] && [ "$cant_intentos" -le "10" ];
-				then 
+					then 
 					echo "Lo lograste, felicidades! Ahora puedes seguir con tu vida promedio"
 			     	else 
 			     		echo "Lo lograste despues de tan solo... $cant_intentos intentos... bueno siempre puede ser peor no?"
@@ -28,7 +30,7 @@ do
 		else 
 			if [ "$num_aleatorio" -gt "$numero" ];
 				then 
-					echo "Te quedaste un poco cortx... Intenta con un numero mas grande"
+				echo "Te quedaste un poco cortx... Intenta con un numero mas grande"
 			else
 				echo "Te pasaste... Intenta con un numero mas chico"
 			fi
--- bot/bot.sh
+++ bot/bot.sh
@@ -1,50 +1,63 @@
 #Mensaje buenos dias
+clear
 echo "Buenos días $USER!"
 
-#Menu opciones
-echo "¿Con que puedo ayudarte?"
-echo "1. Necesito saber la hora"
-echo "2. Me gustaria ver el clima de hoy para Bahia Blanca"
-echo "3. Quiero salir"
-echo "Ingrese una opción y presione enter..."
-
-
-#Eleccion opcion
-read opcion
-clear
+opcion=-1
 
 #Programa
-case $opcion in
-	"1"|"1.")
-		#Determino si es AM o PM - Hay metodos que nos permiten hacer esto directamente desde "date", pero dependen de
-		#la configuración del sistema, y no me pareció correcto modificarla desde un script. Para asegurarme que se muestre
-		#siempre tal cual lo quiero, decidi usar un if para determinar si es AM o PM
-		hora=`date +%H`
-		if [ $hora -lt "12" ]; 
-			then
-			formato="A.M."
-		else 
-			formato="P.M."
-		fi
-		echo "====================="
-		echo "La hora es "$(date +"%I:%M %p") "$formato"
-		echo "====================="
-	;;
+while [ "$opcion" -ne "3" ];
+do
+	#Menu opciones
+	echo "-------------------------------------------------------"
+	echo " "
+	echo "¿Con que puedo ayudarte?"
+	echo "1. Necesito saber la hora"
+	echo "2. Me gustaria ver el clima de hoy para Bahia Blanca"
+	echo "3. Quiero salir"
+	echo " "
+	echo "-------------------------------------------------------"
+	echo "Ingrese una opción y presione enter..."
 	
-	"2"|"2.")
-		curl https://wttr.in/Bahia-Blanca
-	;;
 	
-	"3"|"3.")
-		echo "Hasta luego $USER, espero volver a verte pronto!"
-		exit
-	;;
+	#Eleccion opcion
+	read opcion
+	clear
 	
-	*) 
-		echo "Todavia no tengo disponible esa opción :'("
-		echo "Intentá con opción 1 u opción 2"
-	;;
-esac
+	#Diferentes programas
+	case $opcion in
+		"1")
+			#Determino si es AM o PM - Hay metodos que nos permiten hacer esto directamente desde "date", pero dependen de
+			#la configuración del sistema, y no me pareció correcto modificarla desde un script. Para asegurarme que se muestre
+			#siempre tal cual lo quiero, decidi usar un if para determinar si es AM o PM
+			hora=`date +%H`
+			if [ $hora -lt "12" ]; 
+				then
+				formato="A.M."
+			else 
+				formato="P.M."
+			fi
+			echo "====================="
+			echo "La hora es "$(date +"%I:%M %p") "$formato"
+			echo "====================="
+		;;
+		
+		"2")
+			echo "================================="
+			curl https://wttr.in/Bahia-Blanca?0
+			echo "================================="
+		;;
+		
+		"3")
+			echo "Hasta luego $USER, espero volver a verte pronto!"
+			exit
+		;;
+		
+		*) 
+			echo "Todavia no tengo disponible esa opción :'("
+			echo "Intentá con opción 1 u opción 2"
+		;;
+	esac
+done
 
 echo "Espero haber ayudado :) mucha suerte en tu día! Adiós"
 		
